9c4edc8f2c1226aab02fba71692b59c0c3c91f6a
EmmaMaltseva <emmamaltseva0@gmail.com>
2024-04-16 11:08:51 +0300

    добавила listview

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ItCoursesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//StatelessWidget - нет своего состояния
+//StatefulWidget - есть свое состояние
+class ItCoursesApp extends StatelessWidget {
+  const ItCoursesApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //метод build, чтобы что-то отстроить
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //общие стили для виджетов, например scaffoldBackgroundColor
+        primaryColor: const Color.fromRGBO(112, 130, 255, 100),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'it.courses'),
+      home: const MyHomePage(title: 'it.courses'), //базовая страница приложения
     );
   }
 }
@@ -33,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    //обновляем state виджета (что и в какой момент нужно перестроить)
     setState(() {
       _counter++;
     });
@@ -40,44 +44,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text('Мои курсы',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 20,
-                  )),
-              Text('Рекомендуем для тебя',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 15,
-                  )),
-              Card(
-                  color: Colors.white,
-                  elevation: 0.06,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            height: 30,
-                            color: Colors.white,
-                            child: Image.network(
-                                "https://digital-academy.ru/foto/school/skypro-2.png"),
-                          ),
-                          Text('Профессия разработчик на Python')
-                        ],
-                      )
-                    ],
-                  ))
-            ],
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            Color.fromARGB(235, 201, 201, 255),
+            Color.fromARGB(255, 255, 255, 255)
+          ])),
+      child: Scaffold(
+        //чаще всего отдельный экран
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          title: Text(widget.title),
+          leading: const Icon(Icons.computer),
+        ),
+        body: ListView.builder(
+          itemCount: 30,
+          itemBuilder: (context, i) => const ListTile(
+            leading: Icon(Icons.icecream_outlined),
+            title: Text('Курс'),
           ),
         ),
       ),
