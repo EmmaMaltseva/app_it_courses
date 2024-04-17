6829ee98839f009d64c8bd44f5529e3421a015e6
EmmaMaltseva <emmamaltseva0@gmail.com>
2024-04-17 17:59:26 +0300

    Сделан набросок главного экрана

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const ItCoursesApp());
@@ -9,16 +13,15 @@ void main() {
 //StatefulWidget - есть свое состояние
 class ItCoursesApp extends StatelessWidget {
   const ItCoursesApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     //метод build, чтобы что-то отстроить
     return MaterialApp(
       theme: ThemeData(
-        //общие стили для виджетов, например scaffoldBackgroundColor
-        primaryColor: const Color.fromRGBO(112, 130, 255, 100),
-        useMaterial3: true,
-      ),
+          //общие стили для виджетов, например scaffoldBackgroundColor
+          primaryColor: const Color.fromRGBO(112, 130, 255, 100),
+          useMaterial3: true,
+          fontFamily: 'Inter'),
       home: const MyHomePage(title: 'it.courses'), //базовая страница приложения
     );
   }
@@ -27,51 +30,155 @@ class ItCoursesApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    //обновляем state виджета (что и в какой момент нужно перестроить)
-    setState(() {
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.all(24.0),
+          sliver: SliverAppBar(
+            leading: SvgPicture.asset(
+              'assets/svg/logo.svg',
+              height: 38,
+            ),
+            title: const Text('it.courses'),
+            titleTextStyle:
+                const TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+            surfaceTintColor: Colors.transparent,
+            pinned: true,
+            snap: true,
+            floating: true,
+          ),
+        ),
+        /*const SliverToBoxAdapter(
+            child:
+                SizedBox(height: 16)), //расстояние между апбаром и сливерлистом*/
+        const SliverToBoxAdapter(
+          child: PreferredSize(
+            preferredSize: Size.fromHeight(70),
+            child: SearchButton(), //extract widget строки поиска
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.all(24.0),
+            child: DefaultTextStyle(
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),
+              child: Text('Мои курсы'),
+            ),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 16)),
+        SliverList.builder(
+            itemBuilder: (context, index) =>
+                const RhymeListCard() //extract widget карточка рифма с лайком
+            )
+      ],
+    );
   }
+}
+
+//контенер для стилизации карточки истории рифмы и карточки рифмы с лайком
+class BaseContainer extends StatelessWidget {
+  const BaseContainer({
+    super.key,
+    required this.child,
+    required this.width,
+    this.margin, //если свойство не нужно везде задавать
+    this.padding = const EdgeInsets.only(left: 12),
+  });
+
+  final double width;
+  final Widget child;
+  final EdgeInsets? margin; //если свойство не нужно везде задавать
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-            Color.fromARGB(235, 201, 201, 255),
-            Color.fromARGB(255, 255, 255, 255)
-          ])),
-      child: Scaffold(
-        //чаще всего отдельный экран
-        ///изменения
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          bottomOpacity: 0.0,
-          elevation: 0.0,
-          title: Text(widget.title),
-          leading: const Icon(Icons.computer),
-        ),
-        body: ListView.builder(
-          itemCount: 30,
-          itemBuilder: (context, i) => const ListTile(
-            leading: Icon(Icons.icecream_outlined),
-            title: Text('Курс'),
+      width: width,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+          color: theme.cardColor, borderRadius: BorderRadius.circular(12)),
+      child: child,
+    );
+  }
+}
+
+//виджет с карточками рифмы и лайка
+class RhymeListCard extends StatelessWidget {
+  const RhymeListCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return BaseContainer(
+      margin: EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 10),
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Image(
+            height: 100,
+            image: NetworkImage(
+                'https://all-events.ru/upload/iblock/a60/it8415tgvka1gch4hvu57k23d4qhllwd.png'),
           ),
-        ),
+          Text('Курс', style: theme.textTheme.bodyMedium),
+          IconButton(
+              onPressed: () {},
+              icon:
+                  Icon(Icons.favorite, color: theme.hintColor.withOpacity(0.3)))
+        ],
       ),
     );
+  }
+}
+
+//виджет строки поиска рифмы
+class SearchButton extends StatelessWidget {
+  const SearchButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+        width: double.infinity, //во всю ширину
+        margin: EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 8),
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: theme.hintColor.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(16)),
+        child: Row(
+          children: [
+            Icon(
+              Icons.search,
+              color: Colors.grey[700],
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            DefaultTextStyle(
+              style: TextStyle(),
+              child: Text(
+                'Поиск курса...',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: theme.hintColor.withOpacity(0.5),
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+          ],
+        ));
   }
 }
