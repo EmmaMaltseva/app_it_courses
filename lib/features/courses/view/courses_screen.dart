72dadbf41db24d04060a4292058bbed20a50621e
EmmaMaltseva <emmamaltseva0@gmail.com>
2024-04-21 20:33:04 +0300

    вернула customscrollview, Добавила slivers, заменила appbar на sliverappbar, listview на sliverlist

--- lib/features/courses/view/courses_screen.dart
+++ lib/features/courses/view/courses_screen.dart
@@ -26,7 +26,7 @@ class _CoursesScreenState extends State<CoursesScreen> {
         137900,
         'https://sky.pro/courses/programming/python-web-course-video'),
     CourseModel(
-        "Профессия разработчик на Python",
+        "Большая разработчик на Python",
         "assets/svg/geekbrains.svg",
         'онлайн вебинары',
         '10 месяцев',
@@ -34,7 +34,7 @@ class _CoursesScreenState extends State<CoursesScreen> {
         137900,
         'https://sky.pro/courses/programming/python-web-course-video'),
     CourseModel(
-        "Профессия разработчик на Python",
+        "АААА разработчик на Python",
         "assets/svg/skillbox.svg",
         'онлайн вебинары',
         '10 месяцев',
@@ -75,168 +75,163 @@ class _CoursesScreenState extends State<CoursesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 232, 232, 255),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 232, 232, 255),
-          elevation: 0.0,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Мои курсы",
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),
+        body: CustomScrollView(slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(24.0),
+            sliver: SliverAppBar(
+              pinned: true,
+              snap: true,
+              floating: true,
+              backgroundColor: Color.fromARGB(255, 232, 232, 255),
+              elevation: 0.0,
+              surfaceTintColor: Colors.transparent,
+              title: Text('it.courses'),
+              leading: SvgPicture.asset(
+                "assets/svg/logo.svg",
               ),
-              const SizedBox(
-                height: 20.0,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(80),
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 12, top: 12),
+                  width: double.infinity,
+                  child: TextField(
+                    onChanged: (value) => updateList(value),
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color.fromARGB(255, 255, 255, 255),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(24),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: "Поиск курса...",
+                        prefixIcon: const Icon(Icons.search)),
+                  ),
+                ),
               ),
-              TextField(
-                onChanged: (value) => updateList(value),
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color.fromARGB(255, 255, 255, 255),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: BorderSide.none,
-                    ),
-                    hintText: "Поиск курса...",
-                    prefixIcon: const Icon(Icons.search)),
+            ),
+          ),
+          SliverList.builder(
+            itemCount: display_list.length,
+            itemBuilder: (context, index) => Container(
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(10, 0, 0, 0),
+                    blurRadius: 13.0,
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Expanded(
-                child: display_list.length == 0
-                    ? const Center(
-                        child: Text("Результат не найден",
-                            style: TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.normal)),
-                      )
-                    : ListView.builder(
-                        itemCount: display_list.length,
-                        itemBuilder: (context, index) => Card(
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(0.0),
+              child: Card(
+                margin: const EdgeInsets.symmetric(horizontal: 24)
+                    .copyWith(bottom: 20),
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SvgPicture.asset(
+                            display_list[index].logo!,
+                            width: 100,
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    SvgPicture.asset(
-                                      display_list[index].logo!,
-                                      width: 100,
-                                    ),
-                                    Flexible(
-                                      child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 15.0),
-                                        child: Text(
-                                          display_list[index].title!,
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.w300,
-                                              fontSize: 15),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text('Формат: '),
-                                        Text(
-                                          '${display_list[index].format!}',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text('Длительность: '),
-                                        Text(
-                                          '${display_list[index].dlitelnost!}',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text('Интенсивность: '),
-                                        Text(
-                                          '${display_list[index].intensivnost!}',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text('Стоимость: '),
-                                        Text(
-                                          '${display_list[index].price!}',
-                                          style: TextStyle(
-                                              fontSize: 13,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    ElevatedButton(
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Icon(
-                                            Icons.arrow_circle_right_outlined,
-                                            color: Colors.white,
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                left: 8.0),
-                                            child: Text(
-                                              "На сайт",
-                                              style: TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            Color.fromARGB(255, 130, 112, 255),
-                                        elevation: 0,
-                                      ),
-                                      onPressed: () {},
-                                    ),
-                                  ],
-                                )
-                              ],
+                          Flexible(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: Text(
+                                display_list[index].title!,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 15),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-              )
-            ],
-          ),
-        ));
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('Формат: '),
+                              Text(
+                                '${display_list[index].format!}',
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text('Длительность: '),
+                              Text(
+                                '${display_list[index].dlitelnost!}',
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text('Интенсивность: '),
+                              Text(
+                                '${display_list[index].intensivnost!}',
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text('Стоимость: '),
+                              Text(
+                                '${display_list[index].price!}',
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.arrow_circle_right_outlined,
+                                  color: Colors.white,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    "На сайт",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 130, 112, 255),
+                              elevation: 0,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]));
   }
 }
 
