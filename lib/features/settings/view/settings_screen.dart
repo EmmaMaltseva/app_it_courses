e7dceb9fd4d256edbf5aba5ebecb34e89a95978c
EmmaMaltseva <emmamaltseva0@gmail.com>
2024-04-30 15:39:40 +0300

    добавила разделы настроек

--- lib/features/settings/view/settings_screen.dart
+++ lib/features/settings/view/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_it_courses/features/settings/widgets/widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -33,9 +34,36 @@ class SettingsScreen extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
+            child: SettingsToggleCard(
+              title: 'Тёмная тема',
+              value: false,
+              onChanged: (value) {},
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SettingsToggleCard(
+              title: 'Уведомления',
+              value: true,
+              onChanged: (value) {},
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SettingsToggleCard(
+              title: 'Разрешить аналитику',
+              value: false,
+              onChanged: (value) {},
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 16,
+            ),
+          ),
+          SliverToBoxAdapter(
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 24).copyWith(bottom: 10),
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              margin: const EdgeInsets.symmetric(horizontal: 24)
+                  .copyWith(bottom: 10),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
               width: double.infinity,
               decoration: BoxDecoration(
                   color: Colors.white, borderRadius: BorderRadius.circular(10)),
@@ -43,61 +71,19 @@ class SettingsScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Тёмная тема',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    'Поддержка',
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w500),
                   ),
-                  CupertinoSwitch(
-                    value: true,
-                    onChanged: (v) {},
+                  Icon(
+                    Icons.message,
+                    size: 32,
+                    color: Color.fromARGB(255, 130, 112, 255),
                   )
                 ],
               ),
             ),
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 24).copyWith(bottom: 10),
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Уведомления',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  CupertinoSwitch(
-                    value: true,
-                    onChanged: (v) {},
-                  )
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 24).copyWith(bottom: 10),
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Разрешить аналитику',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  CupertinoSwitch(
-                    value: true,
-                    onChanged: (v) {},
-                  )
-                ],
-              ),
-            ),
-          )
         ]));
   }
 }
